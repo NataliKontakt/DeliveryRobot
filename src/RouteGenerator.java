5602d02bda09030d533e750d73ef00f6207ce467
natali-kontakt <natalikontaktgood@gmail.com>
2025-03-06 17:50:41 +0500

    refactoring

---
+++ src/RouteGenerator.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class RouteGenerator {
+    public static String generate(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
+    }
+}
